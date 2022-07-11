24a032e8e224e17d30763643d91381dc0ec4bdd9
eb <eiler.byberi@gmail.com>
2022-07-11 10:39:05 -0700

    Make the #add_symbol method shorter

--- board.rb
+++ board.rb
@@ -1,5 +1,6 @@
 class Board
   attr_accessor :board, :board_layout
+  attr_reader :row_separator, :column_separator
 
   def initialize
     @board = Array.new(10)
@@ -19,35 +20,10 @@ class Board
 
   def add_symbol(move, symbol)
     move = move.to_i
+    board[move] = symbol
+  end
 
-    case move
-    when 1
-      board[1] = symbol
-      print_board
-    when 2
-      board[2] = symbol
-      print_board
-    when 3
-      board[3] = symbol
-      print_board
-    when 4
-      board[4] = symbol
-      print_board
-    when 5
-      sel.board[5] = symbol
-      print_board
-    when 6
-      board[6] = symbol
-      print_board
-    when 7
-      board[7] = symbol
-      print_board
-    when 8
-      board[8] = symbol
-      print_board
-    when 9
-      board[9] = symbol
-      print_board
-    end
+  def valid_move?(move)
+    
   end
 end

474293abd14b9a9f2db3d6ee7aaab62bcb8e2a68
eb <eiler.byberi@gmail.com>
2022-07-11 14:04:58 -0700

    Add the logic for the #winner? method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,10 +1,11 @@
+# require 'pry-byebug'
 require_relative 'board'
 require_relative 'player'
 
 class TicTacToe
-  attr_accessor :player1, :player2
-  
-  @@players = []
+  attr_accessor :player1, :player2, :game_board, :winner
+
+  LINES = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [4, 5, 6], [7, 8, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7]]
 
   def initialize
     puts 'First player name?'
@@ -13,80 +14,38 @@ class TicTacToe
     puts 'Second player name?'
     name = gets.chomp
     @player2 = Player.new(name)
+    @game_board = Board.new
+    @winner = false
   end
 
   def play_game
-    
-    board = Board.new
-    board.print_board
-    winner = false
-    while winner == false
-      puts "Your turn #{@@players[0].name}"
-      move = gets.chomp.split
-      board.add_symbol(move, @@players[0].symbol)
-      winner = check_winner
-      break if winner == true
+    game_board.print_board
 
-      puts "Your turn #{@@players[1].name}"
-      move = gets.chomp.split
-      board.add_symbol(move, @@players[1].symbol)
-      winner = check_winner
+    until winner == true
+      turn(player1)
+      break if winner?(player1)
+
+      turn(player2)
+      winner?(player2)
     end
   end
 
-  def check_winner
-    winner = false
-    if @board[1] == 'X' && @board[8] == 'X' && @board[15] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[3] == 'X' && @board[10] == 'X' && @board[17] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[5] == 'X' && @board[12] == 'X' && @board[19] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[1] == 'X' && @board[3] == 'X' && @board[5] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[8] == 'X' && @board[10] == 'X' && @board[12] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[15] == 'X' && @board[17] == 'X' && @board[19] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[1] == 'X' && @board[10] == 'X' && @board[19] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[5] == 'X' && @board[10] == 'X' && @board[15] == 'X'
-      puts "#{@@players[0].name} won!"
-      winner = true
-    elsif @board[3] == 'O' && @board[10] == 'O' && @board[17] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    elsif @board[5] == 'O' && @board[12] == 'O' && @board[19] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    elsif @board[1] == 'O' && @board[3] == 'O' && @board[5] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    elsif @board[8] == 'O' && @board[10] == 'O' && @board[12] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    elsif @board[15] == 'O' && @board[17] == 'O' && @board[19] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    elsif @board[1] == 'O' && @board[10] == 'O' && @board[19] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    elsif @board[5] == 'O' && @board[10] == 'O' && @board[15] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    elsif @board[1] == 'O' && @board[8] == 'O' && @board[15] == 'O'
-      puts "#{@@players[1].name} won!"
-      winner = true
-    else
-      winner = false
+  def turn(player)
+    puts "\nYour turn #{player.name}: "
+    move = gets.chomp.to_i
+    game_board.add_symbol(move, player.symbol)
+    player.player_moves << move
+  end
+
+  def winner?(player)
+    player_choices = player.player_moves.permutation(3).to_a
+    player_choices.each do |choice|
+      if LINES.include?(choice)
+        puts "#{player.name} won!"
+        return self.winner = true
+      end
     end
+    self.winner = false
   end
 end
 

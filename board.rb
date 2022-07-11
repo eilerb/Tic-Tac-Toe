7630db98ac0033b6b979ee37dc6b595823dd7262
eb <eiler.byberi@gmail.com>
2022-07-11 11:41:02 -0700

    Fix the board array so that it displays a neat board

--- board.rb
+++ board.rb
@@ -1,28 +1,28 @@
+# frozen_string_literal: true
 class Board
-  attr_accessor :board, :board_layout
+  attr_accessor :board
   attr_reader :row_separator
 
   def initialize
     @board = Array.new(10)
+    for i in (0..9)
+      @board[i] = i
+    end
     @row_separator = '---+---+---'
-    @board_layout = "
-    #{board[1]}   |#{board[2]}   |#{board[3]}
-    #{row_separator}
-    #{board[4]}   |#{board[5]}   |#{board[6]}
-    #{row_separator}
-    #{board[7]}   |#{board[8]}   |#{board[9]}"
   end
 
   def print_board
+    board_layout = "
+     #{board[1]} | #{board[2]} | #{board[3]}
+    #{row_separator}
+     #{board[4]} | #{board[5]} | #{board[6]}
+    #{row_separator}
+     #{board[7]} | #{board[8]} | #{board[9]}"
     puts board_layout
   end
 
   def add_symbol(move, symbol)
-    move = move.to_i
     board[move] = symbol
-  end
-
-  def valid_move?(move)
-    board[move].nil?
+    print_board
   end
 end

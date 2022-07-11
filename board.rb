727d099200d9879daf8eb1011d1980a779855173
eb <eiler.byberi@gmail.com>
2022-07-11 10:10:42 -0700

    Remove the usage of self

--- board.rb
+++ board.rb
@@ -5,10 +5,10 @@ class Board
     @board = Array.new(10)
     @row_separator = '---+---+---'
     @column_separator = '   |   |   |'
-    @board_layout = "        
+    @board_layout = "
     #{board[1]}   |#{board[2]}   |#{board[3]}
     #{row_separator}
-    #{board[4]}   |#{board[5]}   |#{board[6]} 
+    #{board[4]}   |#{board[5]}   |#{board[6]}
     #{row_separator}
     #{board[7]}   |#{board[8]}   |#{board[9]}"
   end
@@ -19,34 +19,34 @@ class Board
 
   def add_symbol(move, symbol)
     move = move.to_i
-    
+
     case move
     when 1
-      self.board[1] = symbol
+      board[1] = symbol
       print_board
     when 2
-      self.board[2] = symbol
+      board[2] = symbol
       print_board
     when 3
-      self.board[3] = symbol
+      board[3] = symbol
       print_board
     when 4
-      self.board[4] = symbol
+      board[4] = symbol
       print_board
     when 5
       sel.board[5] = symbol
       print_board
     when 6
-      self.board[6] = symbol
+      board[6] = symbol
       print_board
     when 7
-      self.board[7] = symbol
+      board[7] = symbol
       print_board
     when 8
-      self.board[8] = symbol
+      board[8] = symbol
       print_board
     when 9
-      self.board[9] = symbol
+      board[9] = symbol
       print_board
     end
   end

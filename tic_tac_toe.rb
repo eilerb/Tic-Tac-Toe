143db3d38806d859bfdd116a6270b1c8e889a94a
eb <eiler.byberi@gmail.com>
2022-07-10 19:13:16 -0700

    Remove player.rb and put the player class inside tic_tac_toe.rb

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,32 @@
 class TicTacToe
-  def board
   
+  def initialize
+
+  end
+
+  def play_game
+    puts 'First player name?'
+    player1 = Player.new(gets.chomp)
+    puts 'Second player name?'
+    player2 = Player.new(gets.chomp)
+  end
+
+  def board
+    
+  end
+
+end
+
+class Player
+  attr_accessor :name
+
+  @@player_count = 0
+
+  def initialize(name)
+    @name = name
+    @@player_count += 1
   end
 end
+
+game = TicTacToe.new()
+game.play_game

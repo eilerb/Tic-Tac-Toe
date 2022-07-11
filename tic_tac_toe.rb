9a754413f36b6c29bad2c82bdaf2134a3a0105bd
eb <eiler.byberi@gmail.com>
2022-07-10 21:19:55 -0700

    Fix the logic for the #check_winner method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -59,39 +59,68 @@ class TicTacToe
       puts "Your turn #{@@players[0].name}"
       move = gets.chomp.split
       print_board(move, @@players[0].symbol)
-      check_winner
+      winner = check_winner
+      break if winner == true
 
       puts "Your turn #{@@players[1].name}"
       move = gets.chomp.split
       print_board(move, @@players[1].symbol)
-      check_winner
+      winner = check_winner
     end
   end
 
   def check_winner
     winner = false
-    if @board[1] && @board[8] && @board[15] == 'X' ||
-       @board[3] && @board[10] && @board[17] == 'X' ||
-       @board[5] && @board[12] && @board[19] == 'X' ||
-       @board[1] && @board[3] && @board[5] == 'X' ||
-       @board[8] && @board[10] && @board[12] == 'X' ||
-       @board[15] && @board[17] && @board[19] == 'X' ||
-       @board[1] && @board[10] && @board[19] == 'X' ||
-       @board[5] && @board[10] && @board[15] == 'X'
+    if @board[1] == 'X' && @board[8] == 'X' && @board[15] == 'X' 
+      puts "#{@@players[0].name} won!"
+      winner = true 
+    elsif @board[3] == 'X' && @board[10] == 'X' && @board[17] == 'X' 
+      puts "#{@@players[0].name} won!"
+      winner = true 
+    elsif @board[5] == 'X' && @board[12] == 'X' && @board[19] == 'X'
+      puts "#{@@players[0].name} won!"
+      winner = true 
+    elsif @board[1] == 'X' && @board[3] == 'X' && @board[5] == 'X'
+      puts "#{@@players[0].name} won!"
+      winner = true 
+    elsif @board[8] == 'X' && @board[10] == 'X' && @board[12] == 'X'
+      puts "#{@@players[0].name} won!"
+      winner = true 
+    elsif @board[15] == 'X' && @board[17] == 'X' && @board[19] == 'X' 
+      puts "#{@@players[0].name} won!"
+      winner = true 
+    elsif @board[1] == 'X' && @board[10] == 'X' && @board[19] == 'X'
+      puts "#{@@players[0].name} won!"
+      winner = true 
+    elsif @board[5] == 'X' && @board[10] == 'X' && @board[15] == 'X'
       puts "#{@@players[0].name} won!"
       winner = true
-    elsif @board[1] && @board[8] && @board[15] == 'O' ||
-          @board[3] && @board[10] && @board[17] == 'O' ||
-          @board[5] && @board[12] && @board[19] == 'O' ||
-          @board[1] && @board[3] && @board[5] == 'O' ||
-          @board[8] && @board[10] && @board[12] == 'O' ||
-          @board[15] && @board[17] && @board[19] == 'O' ||
-          @board[1] && @board[10] && @board[19] == 'O' ||
-          @board[5] && @board[10] && @board[15] == 'O'
+    elsif @board[3] == 'O' && @board[10] == 'O' && @board[17] == 'O' 
+      puts "#{@@players[1].name} won!"
+      winner = true 
+    elsif @board[5] == 'O' && @board[12] == 'O' && @board[19] == 'O'
+      puts "#{@@players[1].name} won!"
+      winner = true 
+    elsif @board[1] == 'O' && @board[3] == 'O' && @board[5] == 'O'
+      puts "#{@@players[1].name} won!"
+      winner = true 
+    elsif @board[8] == 'O' && @board[10] == 'O' && @board[12] == 'O'
+      puts "#{@@players[1].name} won!"
+      winner = true 
+    elsif @board[15] == 'O' && @board[17] == 'O' && @board[19] == 'O' 
+      puts "#{@@players[1].name} won!"
+      winner = true 
+    elsif @board[1] == 'O' && @board[10] == 'O' && @board[19] == 'O'
+      puts "#{@@players[1].name} won!"
+      winner = true 
+    elsif @board[5] == 'O' && @board[10] == 'O' && @board[15] == 'O'
+      puts "#{@@players[1].name} won!"
+      winner = true
+    elsif @board[1] == 'O' && @board[8] == 'O' && @board[15] == 'O'
       puts "#{@@players[1].name} won!"
       winner = true
     else
-      winner
+      winner = false
     end
   end
 end
